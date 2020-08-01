5c05ae4b0e9eda5992121b3bb3828d076c51e7dc
kfleury <killian.fleury@epitech.eu>
2020-08-01 14:43:34 +0200

    remove useless files

--- scr/prepare_my_repo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-blih -u killian.fleury@epitech.eu repository create $1
-blih -u killian.fleury@epitech.eu repository setacl $1 ramassage-tek r
-blih -u killian.fleury@epitech.eu repository getacl $1
